609d72344f57cec60529923bc17fcf8afbfc7a0a
RowickaM <rowickam.23@gmail.com>
2020-02-10 10:44:37 +0100

    Updates

---
+++ precache-manifest.7067a040fd47ccb4bb5f4eb8ff3b515e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9831a469d16b4e9a5592c34e6632a49",
+    "url": "/search-engine-layout/index.html"
+  },
+  {
+    "revision": "3e565e37c7eaef775f09",
+    "url": "/search-engine-layout/static/css/main.cad44cda.chunk.css"
+  },
+  {
+    "revision": "e7424195f2f233a57093",
+    "url": "/search-engine-layout/static/js/2.e734d006.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/search-engine-layout/static/js/2.e734d006.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e565e37c7eaef775f09",
+    "url": "/search-engine-layout/static/js/main.7ebd94fa.chunk.js"
+  },
+  {
+    "revision": "28809592852d6478ab4d",
+    "url": "/search-engine-layout/static/js/runtime-main.c01b1966.js"
+  }
+]);
